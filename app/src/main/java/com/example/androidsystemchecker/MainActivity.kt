e0641e7d5bf15d772ad463090cedf2e02add2417
zahra kazemi <zahra.k199002@gmail.com>
2024-05-24 22:46:39 +0330

    fix some bugs in main activity class

--- app/src/main/java/com/example/androidsystemchecker/MainActivity.kt
+++ app/src/main/java/com/example/androidsystemchecker/MainActivity.kt
@@ -12,16 +12,19 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.work.BackoffPolicy
@@ -45,90 +48,88 @@ class MainActivity : ComponentActivity() {
                 arrayOf(Manifest.permission.POST_NOTIFICATIONS),
                 0
             )
-            super.onCreate(savedInstanceState)
+        }
+        super.onCreate(savedInstanceState)
 
 
-            val fileName = "temp.json"
-            val folder =
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-            val file = File(folder, fileName)
-            if (!file.exists()) {
-                return
-            }
-            val fileReader = FileReader(file)
-            val bufferedReader = BufferedReader(fileReader)
-            var line = bufferedReader.readLine()
-            val logs = mutableListOf<String>()
-            while (line != null) {
-                val jsonObject: JSONObject = JSONObject(line)
-                logs.add(
+        setContent {
+            AndroidSystemCheckerTheme {
+                LaunchedEffect(key1 = Unit) {
+                    val workRequest =
+                        PeriodicWorkRequestBuilder<CustomWorker>(
+                            repeatInterval = 15,
+                            repeatIntervalTimeUnit = TimeUnit.MINUTES,
+                        ).setBackoffCriteria(
+                            backoffPolicy = BackoffPolicy.LINEAR,
+                            duration = Duration.ofSeconds(15)
+                        ).build()
 
-                    jsonObject.get("type").toString() + "." +
-                            jsonObject.get("time").toString() + "." +
-                            jsonObject.get("value").toString()
+                    val workManager =
+                        WorkManager.getInstance(applicationContext)
+                    workManager.enqueue(workRequest)
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    LazyColumn(Modifier.fillMaxHeight(.9f)) {
+                        val fileName = "temp.json"
+                        val folder =
+                            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                        val file = File(folder, fileName)
+                        if (!file.exists()) {
+                            return@LazyColumn
+                        }
+                        val fileReader = FileReader(file)
+                        val bufferedReader = BufferedReader(fileReader)
+                        var line = bufferedReader.readLine()
+                        val logs = mutableListOf<String>()
+                        while (line != null) {
+                            val jsonObject: JSONObject = JSONObject(line)
+                            logs.add(
 
-                )
-                line = bufferedReader.readLine()
-            }
-            setContent {
+                                jsonObject.get("type").toString() + "." +
+                                        jsonObject.get("time").toString() + "." +
+                                        jsonObject.get("value").toString()
+                            )
+                            line = bufferedReader.readLine()
+                        }
+                        logs.reverse()
+                        items(logs) {
+                            val data = it.split(".")
+                            Text(text = data[0], modifier = Modifier.padding(2.dp))
+                            Text(text = data[1], modifier = Modifier.padding(2.dp))
+                            Text(text = data[2], modifier = Modifier.padding(2.dp))
 
-                AndroidSystemCheckerTheme {
-                    LaunchedEffect(key1 = Unit) {
-                        val workRequest =
-                            PeriodicWorkRequestBuilder<CustomWorker>(
-                                repeatInterval = 15,
-                                repeatIntervalTimeUnit = TimeUnit.MINUTES,
-                            ).setBackoffCriteria(
-                                backoffPolicy = BackoffPolicy.LINEAR,
-                                duration = Duration.ofSeconds(15)
-                            ).build()
-
-                        val workManager =
-                            WorkManager.getInstance(applicationContext)
-                        workManager.enqueue(workRequest)
+                        }
                     }
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        LazyColumn {
-                            items(logs){
-                                val data = it.split(".")
-                                Text(text = data[0] , modifier = Modifier.padding(2.dp) )
-                                Text(text = data[1], modifier = Modifier.padding(2.dp) )
-                                Text(text = data[2], modifier = Modifier.padding(2.dp) )
+                    Row() {
+                        Button(onClick = {
+                            Intent(
+                                applicationContext,
+                                ForegroundService::class.java
+                            ).also {
+                                it.action =
+                                    ForegroundService.Actions.START.toString()
+                                startService(it)
+                            }
+                        }) {
+                            Text(text = "Star Service")
+                        }
 
+                        Button(onClick = {
+                            Intent(
+                                applicationContext,
+                                ForegroundService::class.java
+                            ).also {
+                                it.action =
+                                    ForegroundService.Actions.STOP.toString()
+                                startService(it)
                             }
                         }
-                        Row (modifier = Modifier.padding(top = 30.dp )){
-
-                            Button(onClick = {
-                                Intent(
-                                    applicationContext,
-                                    ForegroundService::class.java
-                                ).also {
-                                    it.action =
-                                        ForegroundService.Actions.START.toString()
-                                    startService(it)
-                                }
-                            }) {
-                                Text(text = "Star Service")
-                            }
-
-                            Button(onClick = {
-                                Intent(
-                                    applicationContext,
-                                    ForegroundService::class.java
-                                ).also {
-                                    it.action =
-                                        ForegroundService.Actions.STOP.toString()
-                                    startService(it)
-                                }
-                            }
-                            ) {
-                                Text(text = "Stop Service")
-                            }
+                        ) {
+                            Text(text = "Stop Service")
                         }
                     }
                 }
